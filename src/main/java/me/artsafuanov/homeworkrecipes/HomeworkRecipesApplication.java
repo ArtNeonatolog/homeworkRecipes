5aba1411e5e7a3382a4ba36005a2e61aacb25ba1
Артем <marcus201@mail.ru>
2022-12-24 21:28:57 +0500

    начальный коммит

---
+++ src/main/java/me/artsafuanov/homeworkrecipes/HomeworkRecipesApplication.java
@@ -0,0 +1,13 @@
+package me.artsafuanov.homeworkrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/artsafuanov/homeworkrecipes/HomeworkRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.artsafuanov.homeworkrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
